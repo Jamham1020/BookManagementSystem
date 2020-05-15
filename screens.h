9cc2450b36c15dcddba307c528615e23315c9ec1
Janet Pham <jpham2994@panther.chaffey.edu>
2020-05-14 19:59:22 -0700

    Build initial system

---
+++ screens.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "book.h"
+
+void choice1(book[], int& , int);
+void choice2(book[], int);
+void choice3(book[], int);
+void choice4(book[], int& , int);
+void choice5(book[], int);
+void choice6(book[], int&);
+void choice7(book[], int);
